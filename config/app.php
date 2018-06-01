da0f66429673bcac7d4ed31c986ba01f04bae129
bigrocs <bigrocs@qq.com>
2018-06-01 17:56:07 +0800

    更新 jwt 版本

--- config/app.php
+++ config/app.php
@@ -154,6 +154,7 @@ return [
 
          Laracore\Admin\ServiceProvider::class,
          Laracore\Core\ServiceProvider::class,
+         // Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -212,6 +213,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
 
     ],
 
